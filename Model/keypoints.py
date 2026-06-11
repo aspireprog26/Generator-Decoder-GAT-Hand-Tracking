a0cc8393e1e91b8d1a2dd0bcaecccaf57e4280bc
aspire2026 <mtoppin1@pride.hofstra.edu>
2026-06-11 11:54:01 -0400

    Updated keypoints file for stereo imaging and new environment.yml

--- Model/keypoints.py
+++ Model/keypoints.py
@@ -2,8 +2,7 @@ import torch
 import warnings
 from mmengine.logging import MMLogger
 from mmpose.apis import MMPoseInferencer
-import time 
-
+import time
 # hides python level warnings
 warnings.filterwarnings("ignore")  
 
@@ -13,38 +12,46 @@ MMLogger.get_instance("mmpose", log_level = "ERROR")
 MMLogger.get_instance("mmdet", log_level = "ERROR")
 
 class RTMPose():
-    def __init__(self, frame_in, frame_out, dev = 'cpu'):
+    def __init__(self, frame_dir, dev = 'cpu'):
         self.inferencer = MMPoseInferencer('hand', device = dev)
-        self.frame_in_path = frame_in
-        self.frame_out_path = frame_out
-        self.kp_coords = {}
+        self.frame_dir = frame_dir
     
     def get_keypoints(self):
         result_generator = self.inferencer(
-            self.frame_in_path,
+            [self.frame_dir + "FrameIn/left.jpeg", self.frame_dir + "FrameIn/right.jpeg"],
             return_vis = True,      # includes the visualized image in the output
-            vis_out_dir = self.frame_out_path,     # saved visualized image to this path
+            vis_out_dir = self.frame_dir + "FrameOut",     # saved visualized image to this path
             kpt_thr = 0.3,       # only takes keypoints with >= 30% confidence
             radius = 5,
-            thickness = 2
+            thickness = 2,
+            batch_size = 2
         )
         
-        return next(result_generator)
+        # return all results in the generator
+        results = [] 
+        for result in result_generator:
+            results.append(result)
+
+        return results
     
     def get_coords(self):
-        result = self.get_keypoints()
-        predictions = result['predictions'][0]
-        for coords in predictions:
-            keypoints = coords['keypoints']     # gives (x, y) coordinates of keypoints
+        results = self.get_keypoints()
+        img_coords = []
 
-            for kp_index, kp in enumerate(keypoints):
-                self.kp_coords[kp_index] = kp
-        
-        return self.kp_coords
-    
+        for result in results:      # get each generator result
+            for predictions in result['predictions']:       # get each prediction in the batch per image
+                kp_coords = {}
+                for coords in predictions:      # obtain the coordinates per prediction              
+                    for kp_index, coord in enumerate(coords['keypoints']):      # obtain the index and coordinate for each keypoint
+                        kp_coords[kp_index] = coord
+                img_coords.append(kp_coords)
+
+        return img_coords
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-pose = RTMPose('/home/mrtcloud-1/Downloads/WIN_20260609_11_17_13_Pro.jpg', '/home/mrtcloud-1/Documents/Hand-Tracking-2/VideoTracking/frameout.jpeg', dev = device)
+pose = RTMPose('/home/mrtcloud-1/Documents/Hand-Tracking-2/VideoTracking/', dev = device)
 
+keypoints = pose.get_coords()
 total_time = 0
 for i in range(100):
     t0 = time.time()
