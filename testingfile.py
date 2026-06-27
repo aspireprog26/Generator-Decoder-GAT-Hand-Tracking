fa42e439c6ac7b5c682001a483a1b546dc3f31b1
Michael Toppin <miketengineering@gmail.com>
2026-06-27 01:26:24 -0400

    New path for engine

--- testingfile.py
+++ testingfile.py
@@ -298,7 +298,7 @@ class RTMPose:
     
 if __name__ == "__main__":
     # Hardcoded paths for this specific machine/project layout.
-    ENGINE = "/content/rtm_pose.onnx/end2end.engine"
+    ENGINE = "/content/rtmpose_onnx/end2end.engine"
     FRAME_DIR = "/content/Hand-Tracking-2/VideoTracking/"
 
     TRT_LOGGER = trt.Logger(trt.Logger.INFO)
