301984b54128e652ea2c4856a7acb9a7e597fc48
mtgith <122102254+mtgith@users.noreply.github.com>
2026-09-17 17:37:42 -0400

    Update file paths to relative references

--- Model/piceval.py
+++ Model/piceval.py
@@ -15,18 +15,18 @@ from torch.utils.data import DataLoader
 from torch_geometric.data import Batch
 from utils import Stats, procrustesAlign, stereoTransform
 
-sys.path.insert(0, "/home/miket/Documents/Hand-Tracking-2/Keypoints")
-sys.path.insert(0, "/home/miket/Documents/Hand-Tracking-2/Dataset")
+sys.path.insert(0, "/Hand-Tracking-2/Keypoints")
+sys.path.insert(0, "/Hand-Tracking-2/Dataset")
 
 from handedgeindex import hand_edge_index  # type: ignore
 from keypointdetection import HAND_SKELETON, MediaPipe  # type: ignore
 
 sample_eval = True
 
-with open("/home/miket/Documents/Hand-Tracking-2/Model/decpostconfigs.json", "r") as f:
+with open("/Hand-Tracking-2/Model/decpostconfigs.json", "r") as f:
     dec_post_configs = json.load(f)
 
-with open("/home/miket/Documents/Hand-Tracking-2/Model/manoconfigs.json", "r") as f:
+with open("/Hand-Tracking-2/Model/manoconfigs.json", "r") as f:
     mano_configs = json.load(f)
 
 
@@ -195,7 +195,7 @@ def evalModel(sample: Path):
         right_kps, _, _ = pose.get_keypoints(right)
 
         fs = cv2.FileStorage(
-            "/home/miket/Documents/Hand-Tracking-2/Stereo/stereo.yml",
+            "/Hand-Tracking-2/Stereo/stereo.yml",
             cv2.FILE_STORAGE_READ,
         )
 
@@ -275,5 +275,5 @@ if not sample_eval:
     test_loss, test_dist, avg_time = evalModel(None)
     print(f"Test Anatomy Loss {test_loss: .6f} | Test Dist Loss {test_dist: .6f}")
 else:
-    _, _, avg_time = evalModel("/home/miket/Documents/StereoDataset/Noisy/2607.jpg")
+    _, _, avg_time = evalModel("/StereoDataset/Noisy/2607.jpg")
     print(f"Average Time: {avg_time: .6f}")
